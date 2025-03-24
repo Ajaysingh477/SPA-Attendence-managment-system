c4391f98d4475a842bf6fe0e6d56d1d58434f8de
Ajaysingh Rathore <160405574+Ajaysingh477@users.noreply.github.com>
2025-03-24 16:49:48 +0530

    Update attendance.sql

--- attendance.sql
+++ attendance.sql
@@ -1,31 +1,14 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Mar 24, 2025 at 07:07 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `attendance`
---
 
 -- --------------------------------------------------------
 
---
--- Table structure for table `tbl_admin`
---
+
 
 CREATE TABLE `tbl_admin` (
   `admin_id` int(11) NOT NULL,
@@ -33,9 +16,7 @@ CREATE TABLE `tbl_admin` (
   `admin_password` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
---
--- Dumping data for table `tbl_admin`
---
+
 
 INSERT INTO `tbl_admin` (`admin_id`, `admin_user_name`, `admin_password`) VALUES
 (1, 'admin', '$2y$10$D74Zy1qMkATvmGRoVeq7hed4ajWof2aqDGnEaD3yPHABA.p.e7f4u');
@@ -430,6 +411,3 @@ ALTER TABLE `tbl_teacher`
   MODIFY `teacher_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
